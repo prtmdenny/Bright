5873e47b866c8aefab6968590d51e5786b21abff
prtmdenny <prtmdenny@gmail.com>
2025-03-04 15:25:21 +0700

    update dashboard

---
+++ src/components/layout/navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="w-100 p-2 d-flex align-items-center border-bottom justify-content-between">
+    <div class="logo p-2 d-flex align-items-center">
+      <div class="d-flex border border-primary rounded-circle">
+        <img width="60" class="rounded" src="@/assets/factory.png" alt="Logo" />
+      </div>
+      <div class="d-block ms-3">
+        <h3 class="m-0">Bright</h3>
+        <p class="m-0">Company</p>
+      </div>
+    </div>
+    <div class="d-flex">
+      <a href="">
+        <img width="35" class="rounded" src="@/assets/control.png" alt="Logo" />
+      </a>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
---
+++ src/components/layout/sidebar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="border-end bg-white min-vh-100  w-25">
+    <div class="list-group list-group-flush">
+      <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!"
+        >Kelola</a
+      >
+      <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!"
+        >Usaha</a
+      >
+      <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Laba</a>
+    </div>
+  </div>
+</template>
